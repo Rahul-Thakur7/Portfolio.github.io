e064296a2900abdaa707e2b6580030f3643d0661
Rahul thakur <69378114+Rahul-Thakur7@users.noreply.github.com>
2022-01-31 19:45:07 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,37 @@
+var data = {
+  
+  about : [
+    {name: 'About', value: 'This is Rahul age 22 graduate in Bachelor of Computer Science passionate and enthusiast in Cybersecurity and infosec field, love to play with cmd on TERMINAL and this is the story that how i get into cybersecurity learning journey ..in 1st year of graduation i hate when teachers just copye paste code on board and say COPY TTHAT!! during practical students only copy paste code from bord while i doing my practical i learn how to access other pc in same LAN network , how to shutdown them ,how to get inside in database ,how share data ,how can i share massages , learn networking oncisco packet tracer ,Networking commands and many more,then in 2nd year i learn how website work ,how to create website using webprogramming html,css,js, how to store data in database then i learn mysql,sql, how server work i learn localhost xamp,wamp ,apache make couple of projects then in 3rd year my intrest inclined in Hacking of cybersecurity subject and here is my hacking learning journey started after creating website i started learning how to hack them ..starting learning tools ,how to recon,started reading infosec writeups ,bug bounty reports, because idont have much money to buy costly hacking books ,course ..so to grab them for free i learn some dorking inshort googlehacking and now i learn cybersecurity skill on @tryhackme to gain real hacking experience and play CTF'},
+  ],
+  whoami: [
+    {name: 'Name', value: 'Rahul thakur'},
+  ],
+  skills: [
+    {name: 'Skill', value: 'I am experienced with Python and the web technologies like  HTML, CSS, js,  Node.js backends frameworks as Expressjs, React, database as MongoDB, MySQL, and SQLite and in hacking recon tools Nmap,Sqlmap,wireshark,metasploit,nessus,nikto, and some open source tools like amass ,subfinder ,httpx etc learning OWASP top10 '},
+    
+    
+  
+  ],
+  from: [
+    {name: 'localhost', value: '127.0.0.1 ...just kidding im from Mumbai 😃!!'}
+  ],
+  projects: [
+    {name: 'Network', value:  'https://network-webapplication.herokuapp.com/'},
+    {name: 'Portscanner', value: 'https://github.com/Rahul-Thakur7/Portscann'},
+    {name: 'Web-crawler', value: 'https://github.com/Rahul-Thakur7/web-crawler'},
+    {name: 'Open ports', value: 'https://github.com/Rahul-Thakur7/Open-ports'},
+    {name: 'WhtsappAuto', value: 'https://github.com/Rahul-Thakur7/WhatsApp-Automation'},
+    {name: 'Elibrary', value: 'https://github.com/Rahul-Thakur7/Elibraryin-python/tree/main/vspython'},
+  ],
+  resume: 'resume.pdf',
+  socials: [
+    {name: 'LinkedIn', value: 'https://www.linkedin.com/in/rahul-thakur7/'},
+    {name: 'GitHub', value: 'https://github.com/Rahul-Thakur7'},
+    {name: 'Instagram', value: 'https://www.instagram.com/coder_rahult/'},
+    {name: 'Website', value: 'https://fallc0n.blogspot.com/'},
+    {name: 'Twitter', value: 'https://twitter.com/Rahulth18660383'},
+    {name: 'Sololearn', value: 'https://www.sololearn.com/profile/17762612'},
+    {name: 'Tryhackme', value: 'https://tryhackme.com/p/rahulthakur'},
+    
+  ]
+}
